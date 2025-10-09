aba116107225b33ab1e951b1738aa2f755cd9083
Pecosa09 <70953409+Pecosa09@users.noreply.github.com>
2025-10-09 22:38:37 +0400

    6.5 Фикстуры Pytest

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -5,34 +5,12 @@ import pytest
 
 @pytest.mark.regression
 @pytest.mark.courses
-def test_empty_courses_list():
-    with sync_playwright() as playwright:
-        chromium = playwright.chromium.launch(headless=False)
-        context = chromium.new_context()
-        page = context.new_page()
-
-        page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
-        email_input = page.get_by_test_id("registration-form-email-input").locator("input")
-        username_input = page.get_by_test_id("registration-form-username-input").locator("input")
-        password_input = page.get_by_test_id("registration-form-password-input").locator("input")
-        registration_button = page.get_by_test_id("registration-page-registration-button")
-        dashboard = page.get_by_test_id("dashboard-toolbar-title-text")
-
-        email_input.fill("user.name@gmail.com")
-        username_input.fill("username")
-        password_input.fill("password")
-        registration_button.click()
-        context.storage_state(path='browser-state.json')
-
-    with sync_playwright() as playwright:
-        chromium = playwright.chromium.launch(headless=False)
-        context = chromium.new_context(storage_state='browser-state.json')
-        page = context.new_page()
-        page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses')
-        section_title = page.get_by_test_id("courses-list-toolbar-title-text")
-        block_icon = page.get_by_test_id("courses-list-empty-view-icon")
-        block_title = page.get_by_test_id("courses-list-empty-view-title-text")
-        block_description = page.get_by_test_id("courses-list-empty-view-description-text")
+def test_empty_courses_list(chromium_page_with_state):
+        chromium_page_with_state.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses')
+        section_title = chromium_page_with_state.get_by_test_id("courses-list-toolbar-title-text")
+        block_icon = chromium_page_with_state.get_by_test_id("courses-list-empty-view-icon")
+        block_title = chromium_page_with_state.get_by_test_id("courses-list-empty-view-title-text")
+        block_description = chromium_page_with_state.get_by_test_id("courses-list-empty-view-description-text")
 
         expect(section_title).to_be_visible()
         expect(section_title).to_have_text('Courses')
